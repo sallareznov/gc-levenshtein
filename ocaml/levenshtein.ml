77e4b916499d5474330e5bb625f5d07e3c97caa9
sallareznov <diagne14@gmail.com>
2015-12-21 00:42:38 +0100

    :white_check_mark: [OCAML] Unit test for the Levenshtein distance

--- ocaml/levenshtein.ml
+++ ocaml/levenshtein.ml
@@ -56,15 +56,3 @@ let calculate_distance_from_file filepath =
 		done
 	with
 		| End_of_file -> close_in _in
-
-let usage() = Printf.printf "Browse the file README.md at the root of the project to know how to use this program\n"
-
-let _ =
-		if ((Array.length Sys.argv) < 2) then
-			usage()
-		else begin
-			let filepath = Sys.argv.(1)
-			in
-			calculate_distance_from_file filepath;
-			Printf.printf "DONE !"
-		end
---
+++ ocaml/levenshtein_test.ml
@@ -0,0 +1,19 @@
+open OUnit2
+
+let test_same_distance_for_all_techniques test_ctxt =
+  let distance_techniques = [|Levenshtein.recursive_distance; Levenshtein.dynamic_distance|]
+  in
+  for i = 0 to Array.length distance_techniques - 1 do
+    assert_equal 2 (distance_techniques.(i) "this" "has");
+    assert_equal 2 (distance_techniques.(i) "book" "back");
+    assert_equal 3 (distance_techniques.(i) "sitting" "kitten");
+    assert_equal 4 (distance_techniques.(i) "meilenstein" "levenshtein");
+    assert_equal 9 (distance_techniques.(i) "something" "");
+    assert_equal 9 (distance_techniques.(i) "" "something")
+  done
+
+let suite =
+"levenshtein_test">:::
+ ["test_same_distance_for_all_techniques">:: test_same_distance_for_all_techniques]
+
+let () = run_test_tt_main suite
