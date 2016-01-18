eb7d9659dbf9d6c5e4c638d64d251a5534f4d893
sallareznov <diagne14@gmail.com>
2016-01-18 20:45:36 +0100

    ...

---
+++ ruby/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+jobs &>/dev/null
+ruby Main.rb ../dictionary_EN.txt &
+new_job_started="$(jobs -n)"
+if [ -n "$new_job_started" ];then
+    VAR=$!
+else
+    VAR=
+fi
+echo $VAR
+../../powerapi-iagl-3.3/bin/powerapi 60 500 $VAR
+mv powerapi.out ../output/powerapi_ruby.out
